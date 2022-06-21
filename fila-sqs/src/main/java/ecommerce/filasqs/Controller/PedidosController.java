cd82b21c45264b3efe161fc73d579995d6bb5e6b
inisap <57592705+inisap@users.noreply.github.com>
2022-06-20 23:54:42 -0300

    ajustes controller

--- fila-sqs/src/main/java/ecommerce/filasqs/Controller/PedidosController.java
+++ fila-sqs/src/main/java/ecommerce/filasqs/Controller/PedidosController.java
@@ -5,23 +5,24 @@ package ecommerce.filasqs.Controller;
 import ecommerce.filasqs.Contract.Pedido;
 import ecommerce.filasqs.service.EnviaPedidoDeCompra;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("pedidos")
+@RequestMapping("/v1")
 public class PedidosController {
 
     @Autowired
     EnviaPedidoDeCompra enviaPedidoDeCompra;
 
-    @PostMapping("/{numero_pedido}")
-    public Pedido realizaPedido(
-            @PathVariable int numero_pedido,
+    @PostMapping("/pedidos")
+    public ResponseEntity<Object> realizaPedido(
             @RequestBody Pedido pedido) {
 
         enviaPedidoDeCompra.enviaPedido(pedido);
 
-        return null;
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
 }
